e90a13cbb6178c8e2d1372f2c6c9d8bf73b035a8
Spencer Keith <skeith@ecec.com>
2021-08-12 13:04:19 -0500

    GUI logo update

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,11 +1,9 @@
-import java.io.IOException;
-
 /**
  * This class is the main class from which to execute this program using command line arguments.
  */
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         LibraryManagementTool libraryManagementTool = new LibraryManagementTool();
         GUI gui = new GUI(libraryManagementTool);
